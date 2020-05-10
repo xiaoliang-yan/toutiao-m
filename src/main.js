b4eb346a36cb2dc0e6e9df842102c9ffe07f516b
xiaoliang-yan <1799255243@qq.com>
2020-05-10 15:48:05 +0800

    项目初始目录结构

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
+// 创建Vue根实例，将router，store配置到根实例中
+// 把App根组件渲染到#app节点
 new Vue({
   router,
   store,
