798dfd7af97f96a3ac71ed104e273731fb4bee8d
xiaoliang-yan <1799255243@qq.com>
2020-05-12 00:26:35 +0800

    项目初始化-autoprefixer的配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,13 @@
+/**
+ * psotcss的配置文件
+ * postcss事基于node.js运行的一个处理css的工具
+ * 所以它的配置文件也是运行在node.js中
+ */
 module.exports = {
   plugins: {
-    autoprefixer: {
-      browsers: ['Android >= 4.0', 'iOS >= 8']
-    },
+    // autoprefixer: {
+    //   browsers: ['Android >= 4.0', 'iOS >= 8']
+    // },
     'postcss-pxtorem': {
       rootValue: 37.5,
       propList: ['*']
