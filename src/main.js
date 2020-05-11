14d3b7e0c9f3e8200036de68642363f9a507626b
xiaoliang-yan <1799255243@qq.com>
2020-05-11 21:14:44 +0800

    项目初始化-导入vant组件库

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import store from './store'
 // 加载全局样式
 import './styles/index.less'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 全局注册vant中的组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 // 创建Vue根实例，将router，store配置到根实例中
