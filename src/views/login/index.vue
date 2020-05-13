4bfa70ddac94c691f9ddd8d73f8671c7f7836557
xiaoliang-yan <1799255243@qq.com>
2020-05-13 16:16:06 +0800

    登录注册-页面布局-样式调整

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,27 +1,38 @@
 <template>
   <div class="login-container">
-    <van-nav-bar title="登录" left-arrow @click-left="$router.back()" />
+    <!-- 导航栏 -->
+    <van-nav-bar
+    class="app-nar-bar"
+    title="注册/登录"
+    left-arrow
+    @click-left="$router.back()" />
+    <!-- /导航栏 -->
     <!-- 登录表单 -->
     <van-cell-group>
       <van-field
       v-model="user.mobile"
-      left-icon="smile-o"
+      icon-prefix="toutiao"
+      left-icon="shouji"
       placeholder="请输入手机号" />
       <van-field
       v-model="user.code"
       clearable
-      left-icon="music-o"
+      icon-prefix="toutiao"
+      left-icon="yanzhengma"
       placeholder="请输入验证码">
       <template #button>
           <van-button
+          class="sead-btn"
           size="small"
           round>发送验证码
           </van-button>
        </template>
       </van-field>
     </van-cell-group>
-    <van-button type="info" block>信息按钮</van-button>
-    <!-- /登录按钮 -->
+    <div class="login-btn-wrap">
+      <van-button class="login-btn" type="info" block>登录</van-button>
+    </div>
+    <!-- /登录表单 -->
   </div>
 </template>
 
@@ -47,4 +58,25 @@ export default {
 </script>
 
 <style scoped lang="less">
+.login-container{
+  .sead-btn{
+    width: 76px;
+    height: 23px;
+    background-color:#ededed;
+    .van-button__text{
+      font-size: 11px;
+      color: #666;
+    }
+  }
+  .login-btn-wrap{
+      padding: 26px 16px;
+      .login-btn{
+        background-color: #6db4fb;
+        border: none;
+        .van-button__text{
+          font-size: 15px;
+        }
+      }
+   }
+}
 </style>
